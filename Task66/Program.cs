a64955fa13cfa454bfa721226053a00827bcacab
Viktor <savskij@bk.ru>
2022-09-21 23:02:49 +0300

    Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,15 @@
+﻿int SumOfMN (int M, int N)
+{
+if (M == N) {
+return M;
+} else if(M < N) {
+return M + SumOfMN(M+1, N);
+} else {
+return N + SumOfMN(M, N+1);
+}
+}
+Console.WriteLine("Введите первое число: ");
+int Fnumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int Snumber = int.Parse(Console.ReadLine());
+Console.WriteLine(SumOfMN(Fnumber, Snumber));
