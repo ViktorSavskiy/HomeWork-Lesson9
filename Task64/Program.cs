c0d483a9c57692e1c71b5fc2473905c7197041e4
Viktor <savskij@bk.ru>
2022-09-21 22:47:18 +0300

    Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,18 @@
+﻿string Prom(int num1, int num2)
+{
+    if (num1 == num2)
+    {
+        return num1.ToString();
+    }
+    return num1 + " " + Prom(num1+1, num2);
+    
+}
+
+
+
+Console.WriteLine("Введите первое число : ");
+int M = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число : ");
+int N = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Prom(M, N));
